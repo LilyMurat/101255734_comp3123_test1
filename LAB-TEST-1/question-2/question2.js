ac4e2431346831c92b01910206755ba8d71279fe
Lale Mulati <100545087+LilyMurat@users.noreply.github.com>
2022-10-06 11:28:26 -0700

    Add files via upload

---
+++ LAB-TEST-1/question-2/question2.js
@@ -0,0 +1,21 @@
+// question 2: Promises
+
+const delayedSuccess = () => {
+    setTimeout(()=>{
+        let success ={'message': 'delayed success!'}
+        console.log(success);
+    }, 500)
+}
+
+const delayedException = () => {
+    setTimeout(() => {
+        try {
+            throw new Error('error: delayed exception!');
+        }catch (e) {
+            console.error(e);
+        }
+    }, 500)
+}
+
+delayedSuccess()
+delayedException()
